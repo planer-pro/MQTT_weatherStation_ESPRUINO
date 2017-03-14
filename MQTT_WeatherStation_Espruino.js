377433354907362e2c165a6f63147c412cffb5fa
planer-pro <planer.reg@gmail.com>
2017-03-15 00:36:11 +0300

    Fixed invalid method reading sensor, add publishing only new measurement, add light indication mode.

--- MQTT_WeatherStation_Espruino.js
+++ MQTT_WeatherStation_Espruino.js
@@ -1,47 +1,61 @@
-I2C1.setup({scl: 5, sda: 4});
+I2C1.setup({scl: 5, sda: 4});//bme280 pins
 
-var bme = require("BME280").connect(I2C1);
+var bme;
 var wifi = require("Wifi");
 
-var server = "serv";
+var server = "m10.cloudmqtt.com";
 var options = {
     client_id: "bme280_sens",
     keep_alive: 60,
-    port: 12345,
+    port: 17012,
     clean_session: true,
-    username: "user",
-    password: "pass",
+    username: "nyfibdqw",
+    password: "UB9U4T-px4Zk",
     protocol_name: "MQTT",
-    protocol_level: 4,
+    protocol_level: 4
 };
 
 var mqtt = require("MQTT").create(server, options);
 
 wifi.on('connected', function (details) {
+    bme = require("BME280").connect(I2C1);
     mqtt.connect();
 });
 
 var id;
 
+var tempOld;
+var pressOld;
+var humOld;
+
 mqtt.on('connected', function () {
     id = setInterval(function () {
         bme.readRawData();
 
-        var temp_cal = bme.calibration_T(bme.temp_raw);
-        var press_cal = bme.calibration_P(bme.pres_raw);
-        var hum_cal = bme.calibration_H(bme.hum_raw);
-        var temp_act = temp_cal / 100.0;
-        var press_act = press_cal / 100.0;
-        var hum_act = hum_cal / 1024.0;
+        var temp_act = (bme.calibration_T(bme.temp_raw) / 100.0).toFixed(2);
+        var hum_act = (bme.calibration_H(bme.hum_raw) / 1024.0).toFixed(0);
+        var press_act = (bme.calibration_P(bme.pres_raw) / 100.0).toFixed(2);
         //var alt_act = press_act * 0.75;//wrong calculate
 
-        mqtt.publish("outdoor/sensors/bme280_temp", temp_act);
-        mqtt.publish("outdoor/sensors/bme280_hum", hum_act);
-        mqtt.publish("outdoor/sensors/bme280_press", press_act);
-    }, 2000);
+        if (temp_act != tempOld) {
+            mqtt.publish("outdoor/sensors/bme280_temp", temp_act);
+            tempOld = temp_act;
+        }
+        if (hum_act != humOld) {
+            mqtt.publish("outdoor/sensors/bme280_hum", hum_act);
+            humOld = hum_act;
+        }
+        if (press_act != pressOld) {
+            mqtt.publish("outdoor/sensors/bme280_press", press_act);
+            pressOld = press_act;
+        }
+
+        digitalPulse(2, 0, [60, 60, 60]);//led indicator MQTT transmit
+    }, 4000);
 });
 
 mqtt.on('disconnected', function () {
     clearInterval(id);
+    digitalWrite(2, 0);//led indicator MQTT disconnected
     mqtt.connect();
 });
